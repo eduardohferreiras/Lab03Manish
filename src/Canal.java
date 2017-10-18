3ca069dec375923306c4cfa7a3ddb6797c5e46a7
Eduardo Henrique <eduardo.hferreiras@gmail.com>
2017-10-18 16:01:49 -0200

    Código do Canal e inicio do código do Decodificador.

--- src/Canal.java
+++ src/Canal.java
@@ -1,2 +1,60 @@
+import java.util.Random;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+
 public class Canal {
+
+    Canal() {
+        rand = new Random();
+        msgProcessada = "";
+    }
+
+    private double _probErro;
+    private String msgProcessada;
+    private Random rand;
+
+    public void setProbErro(double prob) {
+        _probErro = prob;
+    }
+
+    private String processarErroBit(String bit) {
+        if (rand.nextDouble() <= _probErro) {
+            if (bit.equals("1")) {
+                return "0";
+            }
+            return "1";
+        }
+        return bit;
+    }
+
+    public String processarString(String msg){
+
+        for(int i = 0; i < msg.length(); i++)
+        {
+            msgProcessada = msgProcessada.concat(processarErroBit(msg.charAt(i) + ""));
+        }
+        return msgProcessada;
+    }
+
+    public void imprimirResultadoBSC() throws IOException
+    {
+        FileWriter fileWriter = new FileWriter("resultadoBSC.txt", false);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        PrintWriter codifiedFile = new PrintWriter(bufferedWriter);
+        codifiedFile.print(msgProcessada);
+        codifiedFile.close();
+    }
+
+    public static void main(String args[]) throws IOException
+    {
+        String texto = "10101010101010";
+        Canal canal = new Canal();
+        canal.setProbErro(0.5);
+        System.out.println(canal.processarString(texto));
+        canal.imprimirResultadoBSC();
+    }
+
 }
--- src/Decodificador.java
+++ src/Decodificador.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Decodificador {
+    ArrayList<String> erros = new ArrayList<String>();
+    ArrayList<String> sintomas = new ArrayList<String>();
+
+    Decodificador()
+    {
+        preencherArrays();
+    }
+
+    private void preencherArrays ()
+    {
+        erros.add("1000000");
+        erros.add("0100000");
+        erros.add("0010000");
+        erros.add("0001000");
+        erros.add("0000100");
+        erros.add("0000010");
+        erros.add("0000001");
+
+        sintomas.add("111");
+        sintomas.add("101");
+        sintomas.add("110");
+        sintomas.add("011");
+        sintomas.add("100");
+        sintomas.add("010");
+        sintomas.add("001");
+    }
+
+    
 }
